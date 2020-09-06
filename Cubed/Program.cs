1cabc665b40c9fae672ced3a6f93929d25cc1ec2
HamnYce <hamadboa@gmail.com>
2020-09-06 20:45:34 +0300

    refactored code into methods

--- Cubed/Program.cs
+++ Cubed/Program.cs
@@ -4,70 +4,33 @@ using System.Collections.Generic;
 namespace HRprog
 {
 
-
+    
 
     class Program
     {
+        static List<Employee> employeeList = new List<Employee>();
+        public static int ind = 0;
 
         static void Main(string[] args)
         {
-
+            
             //Main containers, than act like registers. set n,a,i then set them to as new employees properties
-            string n;
-            int a;
-            int i;
+            
             string option;
-            List<Employee> employeeList = new List<Employee>();
-            int ind = 0;
-
-            Console.WriteLine("Press,1 to add employee, 2 to view employee,3 to edit, 4 to delete");
+            //List<Employee> employeeList = new List<Employee>();
+            
+            
+            Console.WriteLine("Press,1 to add employee, 2 to display employees,3 to edit, 4 to delete");
             option = Console.ReadLine();
 
 
             switch (option)
             { 
             //add employee
-            
+               
             
                 case "1":
-                    bool choice = true;
-
-                    do
-                    {
-
-
-                        Console.WriteLine("Please input the employees name, age, and ID.");
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadKey();
-                        Console.Clear();
-
-
-                        Console.Write("Please enter ID of the employee: ");
-                        i = Convert.ToInt32(Console.ReadLine());
-
-
-                        Console.Write("Please enter name of the employee: ");
-                        n = Console.ReadLine();
-
-
-                        Console.Write("Please enter age of the employee: ");
-                        a = Convert.ToInt32(Console.ReadLine());
-
-
-
-                        employeeList.Add(new Employee() { Id = i, Name = n, Age = a });
-
-                        foreach (Employee e in employeeList)
-                        {
-                            Console.WriteLine("index: " + ind + " " + e);
-                            ind++;
-                            
-                        }
-
-                        Console.WriteLine("Would you like to add another employee? (true/false)");
-                        choice = Convert.ToBoolean(Console.ReadLine());
-
-                    } while (choice == true);
+                    employeeAdd();
 
                 break;
                     
@@ -97,16 +60,62 @@ namespace HRprog
                                 Console.WriteLine("I didn't quite get that");
                                 break;
             */
-            
+           
 
             
+        public static void employeeAdd()
+        {
 
-        
-        
+            bool choice = true;
+
+            
+                string n;
+                int a;
+                int i;
+                //List<Employee> employeeList = new List<Employee>();
+                
+
+                Console.WriteLine("Please input the employees name, age, and ID.");
+                Console.WriteLine("Press enter to continue");
+                Console.ReadKey();
+                Console.Clear();
+
+
+                Console.Write("Please enter ID of the employee: ");
+                i = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.Write("Please enter name of the employee: ");
+                n = Console.ReadLine();
+
+
+                Console.Write("Please enter age of the employee: ");
+                a = Convert.ToInt32(Console.ReadLine());
 
 
 
-        
+                employeeList.Add(new Employee() { Id = i, Name = n, Age = a });
+
+                foreach (Employee e in employeeList)
+                {
+                    Console.WriteLine("index: " + ind + " " + e);
+                    ind++;
+
+                }
+
+                Console.WriteLine("Would you like to add another employee? (true/false)");
+                choice = Convert.ToBoolean(Console.ReadLine());
+
+            
+
+
+        }
+
+
+
+
+
+
 
 
     }//Program Bracket
